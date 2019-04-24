2023f53204f4c41655ec1fe8466b50be96e7c093
twentySeven7 <simon_huelsing@yahoo.de>
2019-04-24 21:42:09 +0200

    Übungsblatt 1: Fertig, Buttons werden nun wie beim NumPad angeordnet

--- src/Tastatur.java
+++ src/Tastatur.java
@@ -1,8 +1,7 @@
 import java.awt.Color;
+import java.awt.ComponentOrientation;
 import java.awt.Dimension;
 import java.awt.GridLayout;
-import java.awt.Insets;
-
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JPanel;
@@ -30,7 +29,8 @@ public class Tastatur extends JPanel {
  */
 	private void addJButtons() {
 		JButton[] buttons = new JButton[9];
-		for (int i = 1; i < 10; i++) {
+		this.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+		for (int i = 9; i >= 1; i--) {
 			buttons[i - 1] = new JButton(Integer.toString(i));
 			buttons[i - 1].setPreferredSize(new Dimension(50, 50));
 			this.add(buttons[i - 1]);
